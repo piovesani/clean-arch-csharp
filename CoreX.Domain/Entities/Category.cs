bcb290d0ade884c3770590ace1d472b7bb62f95d
Renan Piovesani <renan.piovesani2021@gmail.com>
2024-12-11 20:23:52 -0300

    first commit

---
+++ CoreX.Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreX.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; private set; }
+        public ICollection<Product>? Products { get; private set; }
+
+    }
+}
